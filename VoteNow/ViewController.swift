29afce84f3856dc1e8e182d1e8411c32ed85fe01
username8797 <amishtyagi25@gmail.com>
2020-10-19 20:49:13 -0700

    Initial Commit

---
+++ VoteNow/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VoteNow
+//
+//  Created by Amish Tyagi on 10/19/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
